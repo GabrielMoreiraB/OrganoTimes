f26e362d036e971c054e2765a37c19c3483a8d4a
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-03-21 20:17:21 -0300

    criando form

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -3,9 +3,9 @@ import "./Formulario.css";
 
 const Formulario = () => {
   return (
-    <section>
-      <h2>Preencha os dados para criar o card do colaborador.</h2>
+    <section className="formulario">
       <form>
+        <h2>Preencha os dados para criar o card do colaborador.</h2>
         <CampoTexto label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
