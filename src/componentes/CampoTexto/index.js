36e3b3384b90e4a972032127245c17c554006d93
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-03-22 19:00:06 -0300

    pegando valores

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,16 @@
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+    let valor = 'gabriel';
+    const aoDigitado = (evento) => {
+        valor = evento.target.value;
+        console.log(evento.target.value)
+    }
+
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
